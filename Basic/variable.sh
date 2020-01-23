dc998325a0aaebb9b46636f90be0b63fc46db34e
kanoop640 <knoop640@gmail.com>
2020-01-23 12:14:59 +0530

    [Anoop]Added.VariableProgram

---
+++ Basic/variable.sh
@@ -0,0 +1,8 @@
+echo welcome to shellScripting
+
+echo Bash directory is : $BASH
+echo Bash version is :$BASH_VERSION
+echo Home directory is :$HOME
+echo Present working directory :$PWD
+#name=Anoop
+echo Ending Script.....
