d32c31cd3585eb0f364d6b6169bc6c4d626b611e
kanoop640 <kanoop640@gmail.com>
2020-01-24 19:23:46 +0530

    [Anoop]Added.TwoDArray

---
+++ BridgeLabz/Basic/TwoDArray.sh
@@ -0,0 +1,17 @@
+read -p "Enter the value of row " m
+read -p "Enter the value of coloum" n
+declare -A arr[$m,$n]
+for ((i=0;i<$m;i++ ))
+{
+	for (( j=0;j<$n;j++))
+       {
+	read arr[$i,$j]
+       }
+}
+for ((i=0;i<$m;i++ ))
+{
+	for (( j=0;j<$n;j++))
+       {
+	echo $arr[$i,$j]
+       }
+}
