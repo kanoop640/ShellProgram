4680bd5980d2eed35735b5730aa82e4679b9ac32
Anoop kumar <kanoop6401@gmail.com>
2020-01-26 19:08:16 +0530

    [Anoop]Added.RefactorofStopWatch

--- BridgeLabz/Basic/StopWatch.sh
+++ BridgeLabz/Basic/StopWatch.sh
@@ -1,9 +1,8 @@
-let start_time="$( date +%s)";
+start_time=$( date +%s);
 echo "Welcome to my timer "
 while [ 1 ]
 do
-	let current_time=$( date + %s)
-	let second=$start_time - $current_time;
+	current_time=$( date +%s );
+	let second=$(( $current_time - $start_time ));
 	echo -ne "\r Time :  $second"
-	sleep 1;
 done
